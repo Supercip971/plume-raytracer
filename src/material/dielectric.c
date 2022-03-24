a4f424a9d60a2853b5a6859665beeedfb74748be
Supercip971 <supercyp971@gmail.com>
2022-03-24 19:26:42 +0000

    dielectric: improved code + use powf instead of pow

--- src/material/dielectric.c
+++ src/material/dielectric.c
@@ -5,9 +5,10 @@
 #include "dielectric.h"
 static rt_float schlick(rt_float cosine, rt_float ref_index)
 {
-    rt_float r0 = (1 - ref_index) / (1 + ref_index);
+    rt_float r0 = (1.0 - ref_index) / (1.0 + ref_index);
     r0 = r0 * r0;
-    return r0 + (1 - r0) * pow(1 - cosine, 5);
+    rt_float v = 1 - cosine;
+    return r0 + (1.0 - r0) * powf(v, 5);
 }
 
 bool dieletric_callback(const Ray *r_in, const HitRecord *record, MaterialRecord *mat_record,
@@ -16,7 +17,8 @@ bool dieletric_callback(const Ray *r_in, const HitRecord *record, MaterialRecord
 
     Vec3 direction;
     mat_record->pdf.type = PDF_NONE;
-    rt_float refraction_ratio = record->front_face ? (1 / self->ref_index) : self->ref_index;
+
+    rt_float refraction_ratio = record->front_face ? (1.0f / self->ref_index) : self->ref_index;
     Vec3 unit_direction = vec3_unit(r_in->direction);
 
     rt_float cos_theta = rt_min(vec3_dot(vec3_inv(unit_direction), record->normal), 1);
@@ -36,7 +38,7 @@ bool dieletric_callback(const Ray *r_in, const HitRecord *record, MaterialRecord
     mat_record->scattered.direction = direction;
     mat_record->scattered.origin = record->pos;
     mat_record->scattered.time = r_in->time;
-    mat_record->is_specular = false;
+    mat_record->is_specular = true;
     mat_record->attenuation = vec3_create(1, 1, 1);
     return 1;
 }
